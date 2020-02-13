2e121ab1447dc82d8f036aa2b4ef31f2614af0d4
FrankKamala <frankkamala@gmail.com>
2020-02-13 12:16:30 +0300

    save Restaurant

--- app/src/main/java/com/example/myrestaurants/ui/RestaurantDetailFragment.java
+++ app/src/main/java/com/example/myrestaurants/ui/RestaurantDetailFragment.java
@@ -7,12 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.myrestaurants.R;
 import com.example.myrestaurants.models.Business;
 import com.example.myrestaurants.models.Category;
+import com.example.myrestaurants.models.Constants;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
@@ -26,22 +30,31 @@ import butterknife.ButterKnife;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RestaurantDetailFragment extends Fragment {
+public class RestaurantDetailFragment extends Fragment implements View.OnClickListener {
 
-    @BindView(R.id.restaurantImageView) ImageView mImageLabel;
-    @BindView(R.id.restaurantNameTextView) TextView mNameLabel;
-    @BindView(R.id.cuisineTextView) TextView mCategoriesLabel;
-    @BindView(R.id.ratingTextView) TextView mRatingLabel;
-    @BindView(R.id.websiteTextView) TextView mWebsiteLabel;
-    @BindView(R.id.phoneTextView) TextView mPhoneLabel;
-    @BindView(R.id.addressTextView) TextView mAddressLabel;
-    @BindView(R.id.saveRestaurantButton) TextView mSaveRestaurantButton;
+    @BindView(R.id.restaurantImageView)
+    ImageView mImageLabel;
+    @BindView(R.id.restaurantNameTextView)
+    TextView mNameLabel;
+    @BindView(R.id.cuisineTextView)
+    TextView mCategoriesLabel;
+    @BindView(R.id.ratingTextView)
+    TextView mRatingLabel;
+    @BindView(R.id.websiteTextView)
+    TextView mWebsiteLabel;
+    @BindView(R.id.phoneTextView)
+    TextView mPhoneLabel;
+    @BindView(R.id.addressTextView)
+    TextView mAddressLabel;
+    @BindView(R.id.saveRestaurantButton)
+    TextView mSaveRestaurantButton;
 
     private Business mRestaurant;
 
     public RestaurantDetailFragment() {
         // Required empty public constructor
     }
+
     public static RestaurantDetailFragment newInstance(Business restaurant) {
         RestaurantDetailFragment restaurantDetailFragment = new RestaurantDetailFragment();
         Bundle args = new Bundle();
@@ -68,7 +81,7 @@ public class RestaurantDetailFragment extends Fragment {
 
         List<String> categories = new ArrayList<>();
 
-        for (Category category: mRestaurant.getCategories()) {
+        for (Category category : mRestaurant.getCategories()) {
             categories.add(category.getTitle());
         }
 
@@ -78,9 +91,29 @@ public class RestaurantDetailFragment extends Fragment {
         mPhoneLabel.setText(mRestaurant.getPhone());
         mAddressLabel.setText(mRestaurant.getLocation().toString());
 
+        mWebsiteLabel.setOnClickListener(this);
+        mPhoneLabel.setOnClickListener(this);
+        mAddressLabel.setOnClickListener(this);
+
+        mSaveRestaurantButton.setOnClickListener(this);
+
         return view;
     }
 
+    @Override
+    public void onClick(View v) {
+
+
+
+        if (v == mSaveRestaurantButton) {
+            DatabaseReference restaurantRef = FirebaseDatabase
+                    .getInstance()
+                    .getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
+            restaurantRef.push().setValue(mRestaurant);
+            Toast.makeText(getContext(), "Saved Your Restaurant", Toast.LENGTH_SHORT).show();
+
+        }
+    }
 }
 
 
